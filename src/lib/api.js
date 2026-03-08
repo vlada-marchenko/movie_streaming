58fe1ba80b7e422c862b18975480383e32317abb
vlada-marchenko <vvvvadya44@gmail.com>
2026-03-09 00:42:48 +0100

    add structure

---
+++ src/lib/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+export default api;
