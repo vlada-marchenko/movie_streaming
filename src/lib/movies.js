0ab97d30ce98371226f0332753e6d8dd60c4b345
vlada-marchenko <vvvvadya44@gmail.com>
2026-03-10 04:09:20 +0100

    add series api

--- src/lib/movies.js
+++ src/lib/movies.js
@@ -15,7 +15,6 @@ export async function getPopularMovies(page = 1) {
   }
 }
 
-
 // Get the trending movies on TMDB.
 export async function getTrendingMovies(timeWindow = "day") {
   try {
@@ -42,7 +41,6 @@ export async function getNewMovies(page = 1) {
   }
 }
 
-
 // Get a list of movies ordered by rating.
 export async function getTopRatedMovies(page = 1) {
   try {
@@ -60,77 +58,76 @@ export async function getTopRatedMovies(page = 1) {
 
 // Get the top level details of a movie by ID.
 export async function getMovieDetails(movieId) {
-    try {
-        const res = await api.get(`/movie/${movieId}`);
-        return res.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const res = await api.get(`/movie/${movieId}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 // Get the crew of the movie.
 export async function getMovieCredits(movieId) {
-    try {
-        const res = await api.get(`/movie/${movieId}/credits`);
-        return res.data;
-        } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const res = await api.get(`/movie/${movieId}/credits`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 // Get the user reviews for a movie.
 export async function getMovieReviews(movieId) {
-    try {
-        const res = await api.get(`/movie/${movieId}/reviews`); 
-        return res.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const res = await api.get(`/movie/${movieId}/reviews`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 // Search for movies by their original, translated and alternative titles.
 export async function getSearchMovies(query, page = 1) {
-    try {
-        const res = await api.get("/search/movie", {
-            params: {
-                query,
-                page,
-            },
-        });
-        return res.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const res = await api.get("/search/movie", {
+      params: {
+        query,
+        page,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 // Get the list of official genres for movies.
 export async function getGenres() {
-    try {
-        const res = await api.get("/genre/movie/list");
-        return res.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-
-    }
+  try {
+    const res = await api.get("/genre/movie/list");
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 // Find movies using over 30 filters and sort options.
 export async function getMoviesByGenre(genreId, page = 1) {
-    try {
-        const res = await api.get("/discover/movie", {
-            params: {
-                with_genres: genreId,
-                page,
-            },
-        });
-        return res.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const res = await api.get("/discover/movie", {
+      params: {
+        with_genres: genreId,
+        page,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
---
+++ src/lib/series.js
@@ -0,0 +1,159 @@
+import api from "./api";
+
+// Get a list of tv shows ordered by popularity.
+export async function getPopularSeries(page = 1) {
+  try {
+    const res = await api.get("/tv/popular", {
+      params: {
+        page,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get the trending series on TMDB.
+export async function getTrendingSeries(timeWindow = "day") {
+  try {
+    const res = await api.get(`/trending/tv/${timeWindow}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get the list of official genres for tv shows.
+export async function getGenresTv() {
+  try {
+    const res = await api.get("/genre/tv/list");
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Find tv shows using over 30 filters and sort options.
+export async function getSeriesByGenre(genreId, page = 1) {
+  try {
+    const res = await api.get("/discover/tv", {
+      params: {
+        with_genres: genreId,
+        page,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Search for tv shows by their original, translated and alternative titles.
+export async function getSearchSeries(query, page = 1) {
+  try {
+    const res = await api.get("/search/tv", {
+      params: {
+        query,
+        page,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get a list of series ordered by rating.
+export async function getTopRatedSeries(page = 1) {
+  try {
+    const res = await api.get("/tv/top_rated", {
+      params: {
+        page,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get a list of series that are currently in theatres.
+export async function getNewSeries(page = 1) {
+  try {
+    const res = await api.get("/movie/on_the_air", {
+      params: {
+        page,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get the top level details of a tv show by ID.
+export async function getSeriesDetails(series_id) {
+  try {
+    const res = await api.get(`tv/${series_id}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get the crew of the movie.
+export async function getSeriesCredits(series_id) {
+  try {
+    const res = await api.get(`/tv/${series_id}/credits`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get the user reviews for a movie.
+export async function getSeriesReviews(series_id) {
+  try {
+    const res = await api.get(`/tv/${series_id}/reviews`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+
+
+
+// Get the details of season of a TV show.
+export async function getSeriesDetails(season_number ,series_id) {
+  try {
+    const res = await api.get(`tv/${series_id}/season/${season_number}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Get the details of episode of season of a TV show.
+export async function getEpisodeDetails(episode_number ,season_number ,series_id) {
+  try {
+    const res = await api.get(`tv/${series_id}/season/${season_number}/episode/${episode_number}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
