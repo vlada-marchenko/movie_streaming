3da9ca455c41717270433da9ba5bc557ac619c93
vlada-marchenko <vvvvadya44@gmail.com>
2026-03-14 05:33:28 +0100

    add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from 'next';
-import TanStackProvider from '../components/TanStackProvider/TanStackProvider';
-import './globals.css';
-import { Manrope } from 'next/font/google';
-import { Viewport } from 'next';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import type { Metadata } from "next";
+import TanStackProvider from "../components/TanStackProvider/TanStackProvider";
+import "./globals.css";
+import { Manrope } from "next/font/google";
+import { Viewport } from "next";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
-  viewportFit: 'cover',
+  viewportFit: "cover",
 };
 
 const manrope = Manrope({
-  subsets: ['latin'],
-  weight: ['200', '300', '400', '500', '600', '700', '800'],
-  variable: '--font-manrope',
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-manrope",
+  display: "swap",
 });
 
-
 export const metadata: Metadata = {
-  title: 'StreamVibe',
-  description: 'StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere.',
+  title: "StreamVibe",
+  description:
+    "StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere.",
   openGraph: {
-    title: 'StreamVibe',
-    description: 'StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere.',
-    url: 'https://streamvibe.vercel.app/',
-    siteName: 'StreamVibe',
-    type: 'website'
+    title: "StreamVibe",
+    description:
+      "StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere.",
+    url: "https://streamvibe.vercel.app/",
+    siteName: "StreamVibe",
+    type: "website",
   },
 };
 
@@ -45,10 +45,41 @@ export default function RootLayout({
       <body className={manrope.className}>
         <TanStackProvider>
           <div className="layout-wrapper">
-          <Header />
-          <main>{children}</main>
-          <Footer />
-        </div>
+            <Header />
+            <main>
+              <svg
+                aria-hidden="true"
+                style={{ width: 0, height: 0, position: "absolute" }}
+              >
+                <defs>
+                  <linearGradient
+                    id="mainIconGradient"
+                    x1="0%"
+                    y1="0%"
+                    x2="0%"
+                    y2="100%"
+                  >
+                    <stop
+                      offset="0%"
+                      style={{
+                        stopColor: "rgb(255, 255, 255)",
+                        stopOpacity: 0.4,
+                      }}
+                    />
+                    <stop
+                      offset="100%"
+                      style={{
+                        stopColor: "rgb(255, 255, 255)",
+                        stopOpacity: 0.2,
+                      }}
+                    />
+                  </linearGradient>
+                </defs>
+              </svg>
+              {children}
+            </main>
+            <Footer />
+          </div>
         </TanStackProvider>
         <ToastContainer position="top-center" autoClose={4000} />
       </body>
